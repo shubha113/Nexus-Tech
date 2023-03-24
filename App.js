283bf120a57cf4b6a5e27b918ca4fe19f1383a64
Shubham Kumar Rai <shubhamkumarrai212@gmail.com>
2023-03-24 16:35:02 +0530

    first commit

--- App.js
+++ App.js
@@ -45,7 +45,7 @@ module.exports = allowCors(handler)
 
 
 
-/*app.use(cors({
+app.use(cors({
   origin: 'https://startup-five-puce.vercel.app',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});*/
+});
 
 
 
