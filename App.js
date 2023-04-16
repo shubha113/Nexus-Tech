5307336af13c39d7c5e510ed2f91464a9a0d762f
shubha113 <85664043+shubha113@users.noreply.github.com>
2023-04-16 13:46:57 +0530

    Update App.js

--- App.js
+++ App.js
@@ -26,6 +26,13 @@ app.use(
 );
 
 // Importing & Using Routes
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
+
+// Importing & Using Routes
 import course from "./Routes/CourseRoutes.js";
 import user from "./Routes/UserRoutes.js";
 import other from "./Routes/OtherRoutes.js";
@@ -33,13 +40,5 @@ import other from "./Routes/OtherRoutes.js";
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", other);
-
-export default app;
-
-app.get("/", (req, res) =>
-  res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
-  )
-);
 
 app.use(ErrorMiddleware);
