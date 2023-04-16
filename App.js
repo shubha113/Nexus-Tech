34ee50bb2e85138870cd7b026ca24ef10c66e388
shubha113 <85664043+shubha113@users.noreply.github.com>
2023-04-16 14:19:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: *,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
