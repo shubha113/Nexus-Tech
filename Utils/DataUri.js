5ed69fac5a50dcf6dd7ee494bbbb27b37f4c329b
shubha113 <85664043+shubha113@users.noreply.github.com>
2023-04-16 13:19:54 +0530

    Update DataUri.js

--- Utils/DataUri.js
+++ Utils/DataUri.js
@@ -1,4 +1,4 @@
-import DataUriParser from "DataUri/parser.js";
+import DataUriParser from "datauri/parser.js";
 import path from "path";
 const getDataUri = (file) => {
   const parser = new DataUriParser();
