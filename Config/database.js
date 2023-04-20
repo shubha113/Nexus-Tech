d6cf5433de3b4c4d5e4ffb0e30cd2408f3498072
shubha113 <85664043+shubha113@users.noreply.github.com>
2023-04-20 12:29:01 +0530

    Update database.js

--- Config/database.js
+++ Config/database.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 mongoose.set('strictQuery', false);
 export const connectDB = async () => {
-  const { connection } = await mongoose.connect(process.env.MONGO_URI);
+  const { connection } = await mongoose.connect('mongodb+srv://Skr_2006:Skr2002@cluster0.zpebaut.mongodb.net/startup?retryWrites=true');
   console.log(`MongoDB connected with ${connection.host}`);
 };
+
 
  {/*https://startup-five-puce.vercel.app/  this is for frontend url   http://localhost:4000 */}
